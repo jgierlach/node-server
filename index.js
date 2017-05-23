8e4ee0075204ddc048e94931a1bc48112e427aa3
Jan Gierlach <jegierlach@gmail.com>
2017-05-23 12:37:32 -0500

    remove express routes, and pug views. serve from static dist folder

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.set('views', './views')
-app.set('view engine', 'pug')
-
-app.get('/', function(req, res) {
-  res.render('index', {title: 'My App', message: 'Hello World!!!'});
-})
-app.get('/about', function(req, res) {
-  res.render('about')
-})
+/* adding one static route for the dist folder that pingy creates */
+app.use(express.static('dist'))
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("server listening on port 3000")
