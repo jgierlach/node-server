e9a6ee08dcd6e8361ba10eb0dbd81395c9c5a579
Jan Gierlach <jegierlach@gmail.com>
2017-05-23 12:43:18 -0500

    export production assets

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
-var express=require("express"),app=express();app.set("views","./views"),app.set("view engine","pug"),app.use(express.static("dist")),app.listen(process.env.PORT||3e3,function(){console.log("server listening on port 3000")});
+var express=require("express"),app=express();app.use(express.static("dist")),app.listen(process.env.PORT||3e3,function(){console.log("server listening on port 3000")});
 //# sourceMappingURL=index.js.map
